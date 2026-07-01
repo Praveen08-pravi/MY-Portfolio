56dbcc515a1a01c7bdbc8eb273df6b1f1928894f
Praveen.U <praveenupraveenu98@gmail.com>
2026-07-01 12:13:26 +0530

    updated sendemail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,20 +1,33 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  family: 4, // 🔥 FORCE IPv4 (fix ENETUNREACH issue)
 });
 
 const sendEmail = async ({ to, subject, html }) => {
-  await transporter.sendMail({
-    from: `"Praveen Portfolio" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+  try {
+    await transporter.sendMail({
+      from: `"Praveen Portfolio" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Email error:", error.message);
+    throw error;
+  }
 };
 
 module.exports = sendEmail;
